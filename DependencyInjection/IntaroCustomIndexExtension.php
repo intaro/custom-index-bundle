32b1a351a2aca337e8992a3518bfc0b56c432aef
kiler129 <grzegorz@noflash.pl>
2019-01-23 13:32:59 -0600

    #7: Add support for Symfony 4.x

--- DependencyInjection/IntaroCustomIndexExtension.php
+++ DependencyInjection/IntaroCustomIndexExtension.php
@@ -6,6 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -19,6 +20,9 @@ class IntaroCustomIndexExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        $loader = new XmlFileLoader($container, new FileLocator(dirname(__DIR__).'/Resources/config'));
+        $loader->load('services.xml');
+
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
